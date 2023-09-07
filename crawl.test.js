09ff8d397e470bfd6b40bf58f6245eb5ad88cd1f
kanayofirms <kanyeze@gmail.com>
2023-09-07 08:02:27 +0100

    Run strip trailing slash test

--- crawl.test.js
+++ crawl.test.js
@@ -7,3 +7,13 @@ test('normalizeURL strip protocol', () => {
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+
