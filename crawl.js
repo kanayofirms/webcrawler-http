29eec1cdb4675ead5fbe29488dddafaabf4b2cb0
kanayofirms <kanyeze@gmail.com>
2023-09-09 03:20:28 +0100

    Update and refactor code for functionality

--- crawl.js
+++ crawl.js
@@ -3,8 +3,6 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls
 }
 
-
-
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
