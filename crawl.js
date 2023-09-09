d37e967a23bf70f63b2d7b4a1931783ca438f770
kanayofirms <kanyeze@gmail.com>
2023-09-09 03:56:30 +0100

    Import jsdom and create new instance of the dom for linkElements

--- crawl.js
+++ crawl.js
@@ -1,5 +1,12 @@
+const { JSDOM } = require('jsdom')
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        urls.push(linkElement.href)
+    }
     return urls
 }
 
