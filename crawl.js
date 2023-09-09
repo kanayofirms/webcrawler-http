d1f1b277ae742496659762cbc963e85b01945128
kanayofirms <kanyeze@gmail.com>
2023-09-09 03:15:23 +0100

    Create getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -1,3 +1,10 @@
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    return urls
+}
+
+
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +15,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
