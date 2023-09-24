3ab1556af82e7fb8f0e3a491eb582f53486ae39b
kanayofirms <kanyeze@gmail.com>
2023-09-24 17:02:48 +0100

    Refactor getURLsFromHTML test and update

--- crawl.test.js
+++ crawl.test.js
@@ -2,30 +2,30 @@ const { normalizeURL, getURLsFromHTML } = require('./crawl.js');
 const { test, expect } = require('@jest/globals');
 
 test('normalizeURL strip protocol', () => {
-    const input = 'https://blog.boot.dev/path'
+    const input = 'https://blog.pembo.org/path'
     const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
+    const expected = 'blog.pembo.org/path'
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL strip trailing slash', () => {
-    const input = 'https://blog.boot.dev/path/'
+    const input = 'https://blog.pembo.org/path/'
     const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
+    const expected = 'blog.pembo.org/path'
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL capitals', () => {
-    const input = 'https://BLOG.boot.dev/path/'
+    const input = 'https://BLOG.pembo.org/path/'
     const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
+    const expected = 'blog.pembo.org/path'
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL strip http', () => {
-    const input = 'http://blog.boot.dev/path/'
+    const input = 'http://blog.pembo.org/path/'
     const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
+    const expected = 'blog.pembo.org/path'
     expect(actual).toEqual(expected)
 })
 
@@ -33,16 +33,15 @@ test('getURLsFromHTML', () => {
     const inputHTMLBody = `
 <html>
     <body>
-        <a href="https://blog.boot.dev/">
-            Boot.dev Blog
+        <a href="https://blog.pembo.org/">
+            Pembo.org Blog
         </a>
     </body>
 </html>   
 `
-    const inputBaseURL = "https://blog.boot.dev"
+    const inputBaseURL = "https://blog.pembo.org"
     const actual = getURLsFromHTML(inputHTMLBody,inputBaseURL)
-    const expected = ["https://blog.boot.dev/"]
+    const expected = ["https://blog.pembo.org/"]
     expect(actual).toEqual(expected)
 })
-
 
